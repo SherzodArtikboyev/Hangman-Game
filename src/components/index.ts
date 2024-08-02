99e6b6f45f8779db75c5945244c8c988ff097496
Sherzod <sherzadartikbayev@gmail.com>
2024-08-02 12:50:57 +0500

    first commit

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as HangmanDrawing } from "./hangman-drawing.tsx"
+export { default as HangmanWord } from "./hangman-word.tsx";
+export { default as Keyboard } from "./keyboard.jsx";
+
